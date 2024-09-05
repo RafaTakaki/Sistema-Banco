c94c1088960b79ea1869ce5f0a630c7f57926dec
Rafa Takaki <rafalimatakak@hotmail.com>
2024-09-05 17:46:33 -0300

    atualização etapa 1 - acrescentando OOP

--- SistemaBanco.py
+++ SistemaBanco.py
@@ -1,3 +1,97 @@
+# from abc import ABC, abstractclassmethod,
+# abstractproperty
+# from datetime import datetime
+
+class Cliente:
+    def __init__ (self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adeicionar_conta(self, conta):
+        self.conta.append(conta)
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self.historico = Historico()
+
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls(numero, cliente)
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor):
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
+
+        if excedeu_saldo:
+            print ("Operação falhou! Você não tem saldo suficiente.")
+
+        elif valor > 0 :
+            self.valor -= valor 
+            print ("\n Saque Realizado com sucesso!")
+            return True
+        else:
+            print("\n Operação falhou! O valor informado é invalido")
+            return False
+    
+    def depositar(self, valor):
+        if valor > 0:
+            self._saldo += valor
+            print("\n Depósito realizado com secesso !!")
+        else:
+            print("\n Operação falhou! O valor informado é invalido")
+            return False
+        return True
+
+
+class ContaCorrente(Conta):
+    pass
+
+class Historico:
+    pass
+
+class Transasao(ABC):
+    pass
+
+class Saque(Transasao):
+    pass
+
+class Deposito(Transasao):
+    pass
+
 menu = """
 
 [d] Depositar
@@ -64,5 +158,3 @@ while True:
 
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
-
-        
